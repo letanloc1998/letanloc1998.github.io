1539c08790963e159304e85f49037122a2a5048f
Lê Tấn Lộc <26348718+letanloc1998@users.noreply.github.com>
2021-06-06 16:38:59 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,12 @@
-python -m http.server 8000
+@REM ~ expands the given variable
+@REM d gets the drive letter only
+@REM 0 is the argument you are referencing
+
+@REM the root directory of the disk where the batch file is located
+%~d0
+
+@REM the directory where the batch file is located
+cd %~p0
+
 @REM python -m http.server 8000 --bind 127.0.0.1
+python -m http.server 8000
